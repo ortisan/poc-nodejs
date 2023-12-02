20366e673fc464fe0989d1b4ab53c93a38d635b4
Marcelo Ortiz de Santana <tentativafc@gmail.com>
2023-12-02 17:46:35 -0300

    feat: tempo and grafana configs

--- src/adapter/input/controller/user.controller.ts
+++ src/adapter/input/controller/user.controller.ts
@@ -4,25 +4,23 @@ import { CreateUserDto } from '@/adapter/dto/create-user.dto';
 import { AbstractUserRepository } from '@/domain/repository/user.repository';
 import { IUserSigninUseCase } from '@/domain/usecase/user/user.interface';
 import { Id } from '@/domain/vo/id.vo';
-import { AbstractMetricService } from '@/shared/observability/metric.service';
-import { OtelMethodCounter, TraceService } from 'nestjs-otel';
+import { OtelMethodCounter, Span, TraceService } from 'nestjs-otel';
 
 @Controller('api/user')
 export class UserController {
-  constructor(private readonly traceService: TraceService, private readonly userSigninUseCase: IUserSigninUseCase, private readonly userRepository: AbstractUserRepository) { }
+  constructor(private readonly userSigninUseCase: IUserSigninUseCase, private readonly userRepository: AbstractUserRepository) { }
 
   @Post()
+  @Span('CreateUser')
   @OtelMethodCounter()
   async create(@Body() createUserDto: CreateUserDto): Promise<CreateUserDto> {
-    const currentSpan = this.traceService.getSpan();
-    currentSpan.addEvent('some event');
-    currentSpan.end();
     const user = ApiCreateUserMapper.toDomain(createUserDto);
     const userCreated = await this.userSigninUseCase.execute(user);
     return ApiCreateUserMapper.toDto(userCreated);
   }
 
   @Get(':id')
+  @Span('GetUserById')
   @OtelMethodCounter()
   async getById(@Param('id') id: string): Promise<CreateUserDto> {
     const user = await this.userRepository.findById(new Id({ stringValue: id }));
--- src/infrastructure/observability/tracer.ts
+++ src/infrastructure/observability/tracer.ts
@@ -1,69 +1,69 @@
-// import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
-// import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
-// import { NodeSDK } from '@opentelemetry/sdk-node';
-// import {
-//   BasicTracerProvider,
-//   ConsoleSpanExporter,
-//   SimpleSpanProcessor,
-// } from '@opentelemetry/sdk-trace-node';
-// import { Resource } from '@opentelemetry/resources';
-// import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
-// import {
-//   PeriodicExportingMetricReader,
-//   ConsoleMetricExporter,
-// } from '@opentelemetry/sdk-metrics';
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import { NodeSDK } from '@opentelemetry/sdk-node';
+import {
+  BasicTracerProvider,
+  ConsoleSpanExporter,
+  SimpleSpanProcessor,
+} from '@opentelemetry/sdk-trace-node';
+import { Resource } from '@opentelemetry/resources';
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
+import {
+  PeriodicExportingMetricReader,
+  ConsoleMetricExporter,
+} from '@opentelemetry/sdk-metrics';
 
-// export class Tracer {
-//   private sdk: NodeSDK | null = null;
+export class Tracer {
+  private sdk: NodeSDK | null = null;
 
-//   // url is optional and can be omitted - default is http://localhost:4318/v1/traces
-//   //private exporter = new OTLPTraceExporter({ url: " http://localhost:4318/v1/traces" });
+  // url is optional and can be omitted - default is http://localhost:4318/v1/traces
+  //private exporter = new OTLPTraceExporter({ url: " http://localhost:4318/v1/traces" });
 
-//   // private provider = new BasicTracerProvider({
-//   //   resource: new Resource({
-//   //     [SemanticResourceAttributes.SERVICE_NAME]: 'DEMO-APP',
-//   //   }),
-//   // });
+  // private provider = new BasicTracerProvider({
+  //   resource: new Resource({
+  //     [SemanticResourceAttributes.SERVICE_NAME]: 'DEMO-APP',
+  //   }),
+  // });
 
-//   public init() {
-//     try {
+  public init() {
+    try {
 
-//       const sdk = new NodeSDK({
-//         resource: new Resource({
-//           [SemanticResourceAttributes.SERVICE_NAME]: 'node app',
-//           [SemanticResourceAttributes.SERVICE_VERSION]: '1.0.0',
-//         }),
-//         traceExporter: new ConsoleSpanExporter(),
-//         metricReader: new PeriodicExportingMetricReader({
-//           exporter: new ConsoleMetricExporter(),
-//         }),
-//       });
+      const sdk = new NodeSDK({
+        resource: new Resource({
+          [SemanticResourceAttributes.SERVICE_NAME]: 'node app',
+          [SemanticResourceAttributes.SERVICE_VERSION]: '1.0.0',
+        }),
+        traceExporter: new ConsoleSpanExporter(),
+        metricReader: new PeriodicExportingMetricReader({
+          exporter: new ConsoleMetricExporter(),
+        }),
+      });
 
-//       // // export spans to console (useful for debugging)
-//       // this.provider.addSpanProcessor(
-//       //   new SimpleSpanProcessor(new ConsoleSpanExporter()),
-//       // );
+      // // export spans to console (useful for debugging)
+      // this.provider.addSpanProcessor(
+      //   new SimpleSpanProcessor(new ConsoleSpanExporter()),
+      // );
 
-//       // // export spans to opentelemetry collector
-//       // this.provider.addSpanProcessor(new SimpleSpanProcessor(this.exporter));
-//       // this.provider.register();
+      // // export spans to opentelemetry collector
+      // this.provider.addSpanProcessor(new SimpleSpanProcessor(this.exporter));
+      // this.provider.register();
 
-//       // this.sdk = new NodeSDK({
-//       //   traceExporter: this.exporter,
-//       //   instrumentations: [
-//       //     getNodeAutoInstrumentations({
-//       //       // Lets disable fs for now, otherwise we cannot see the traces we want,
-//       //       // You can disable or enable instrumentation as needed
-//       //       '@opentelemetry/instrumentation-fs': { enabled: false },
-//       //     }),
-//       //   ],
-//       // });
+      // this.sdk = new NodeSDK({
+      //   traceExporter: this.exporter,
+      //   instrumentations: [
+      //     getNodeAutoInstrumentations({
+      //       // Lets disable fs for now, otherwise we cannot see the traces we want,
+      //       // You can disable or enable instrumentation as needed
+      //       '@opentelemetry/instrumentation-fs': { enabled: false },
+      //     }),
+      //   ],
+      // });
 
-//       this.sdk.start();
+      this.sdk.start();
 
-//       console.info('The tracer has been initialized');
-//     } catch (e) {
-//       console.error('Failed to initialize the tracer', e);
-//     }
-//   }
-// }
+      console.info('The tracer has been initialized');
+    } catch (e) {
+      console.error('Failed to initialize the tracer', e);
+    }
+  }
+}
